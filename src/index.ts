ba5c8b0e6f263ec05b3f988e284d2a75c28639da
dani <dhannizd2@gmail.com>
2026-04-21 21:11:23 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/webhook", async (c) => {
+  const body = await c.req.json();
+
+  console.log("Data dari Telegram:", body);
+
+  return c.text("ok");
+});
+
+export default app;
