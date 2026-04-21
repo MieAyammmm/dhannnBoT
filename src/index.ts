de5f70317f5dd038d6b0a8db32779f6ee5c5338d
dani <dhannizd2@gmail.com>
2026-04-21 21:25:01 +0700

    sendMessage

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+const TOKEN = "8626624026:AAEzMxduEuz19kXwC93QvzK9V6E13155x9o";
+
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
@@ -9,7 +11,25 @@ app.get("/", (c) => {
 app.post("/webhook", async (c) => {
   const body = await c.req.json();
 
-  console.log("Data dari Telegram:", body);
+  const message = body.message;
+  const text = message?.text;
+  const chatId = message?.chat?.id;
+
+  if (!chatId) {
+    return c.text("no chat id");
+  }
+
+  // kirim balasan ke Telegram
+  await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: `Kamu bilang: ${text}`,
+    }),
+  });
 
   return c.text("ok");
 });
